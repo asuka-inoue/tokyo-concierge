8624a316ce92f0e275e24c20be927bb10edfef73
asuka-inoue <tohasu081026@gmail.com>
2022-02-17 17:58:18 +0900

    0217

--- js/script.js
+++ js/script.js
@@ -101,40 +101,40 @@ function search_filter(group) {
 }
 });
 
-// var searchItem = '.search__item';   // 絞り込む項目を選択するエリア
-// var listItem = '.works__card';   // 絞り込み対象のアイテム
-// var hideClass = 'is-hide';         // 絞り込み対象外の場合に付与されるclass名
-// var activeClass = 'is-active';     // 選択中のグループに付与されるclass名
+var searchItem = '.faq__searchitem';   // 絞り込む項目を選択するエリア
+var listItem = '.faq__contents';   // 絞り込み対象のアイテム
+var hideClass = 'is-hide';         // 絞り込み対象外の場合に付与されるclass名
+var activeClass = 'is-active';     // 選択中のグループに付与されるclass名
 
-// $(function() {
-//   // 絞り込みを変更した時
-//   $(searchItem).on('click', function() {
-//     $(searchItem).removeClass(activeClass);
-//     var group = $(this).addClass(activeClass).data('group');
-//     search_filter(group);
-//   });
-// });
+$(function() {
+  // 絞り込みを変更した時
+  $(searchItem).on('click', function() {
+    $(searchItem).removeClass(activeClass);
+    var group = $(this).addClass(activeClass).data('group');
+    search_filter(group);
+  });
+});
 
-// /**
-//  * リストの絞り込みを行う
-//  * @param {String} group data属性の値
-//  */
-// function search_filter(group) {
-//   // 非表示状態を解除
-//   $(listItem).removeClass(hideClass);
-//   // 値が空の場合はすべて表示
-//   if(group === '') {
-//     return;
-//   }
-//   // リスト内の各アイテムをチェック
-//   for (var i = 0; i < $(listItem).length; i++) {
-//     // アイテムに設定している項目を取得
-//     var itemData = $(listItem).eq(i).data('group');
-//     // 絞り込み対象かどうかを調べる
-//     if(itemData !== group) {
-//       $(listItem).eq(i).addClass(hideClass);
-//     }
-//   }
-// };
+/**
+ * リストの絞り込みを行う
+ * @param {String} group data属性の値
+ */
+function search_filter(group) {
+  // 非表示状態を解除
+  $(listItem).removeClass(hideClass);
+  // 値が空の場合はすべて表示
+  if(group === '') {
+    return;
+  }
+  // リスト内の各アイテムをチェック
+  for (var i = 0; i < $(listItem).length; i++) {
+    // アイテムに設定している項目を取得
+    var itemData = $(listItem).eq(i).data('group');
+    // 絞り込み対象かどうかを調べる
+    if(itemData !== group) {
+      $(listItem).eq(i).addClass(hideClass);
+    }
+  }
+};
 
 
